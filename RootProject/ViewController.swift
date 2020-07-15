1d0fb6c2d13a36defe1e85f34507a6bb412f5071
버즈.Buzz.김승진 <buzz.kim@insiar.com>
2020-07-15 14:27:17 +0900

    project init

---
+++ RootProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RootProject
+//
+//  Created by Buzz.Kim on 2020/07/15.
+//  Copyright © 2020 INSIAR Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
