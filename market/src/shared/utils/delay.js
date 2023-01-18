7de9b11bdc8c9b43ab30fbb45b5644e3b52fe8a2
Seungjin Kim <seungjin429@gmail.com>
2023-01-18 21:16:17 +0900

    프로젝트 초기화

--- market/src/shared/utils/delay.js
+++
@@ -1,6 +0,0 @@
-export const gDelay = (time = COMMON_DEALY_TIME, value) =>
-  new Promise((resolve) => {
-    setTimeout(() => resolve(value), time);
-  });
-
-export const COMMON_DEALY_TIME = 1500;
